6ac04f09955a922e607b58dac14725a4e85cbd75
louisemogensenn <eaa23lmog@students.eaaa.dk>
2025-04-07 15:18:06 +0200

    Indsætning af logo

--- src/views/Forside.jsx
+++ src/views/Forside.jsx
@@ -5,8 +5,7 @@ export default function Forside() {
     return (
         <>
             <section className={mystyle.forsideTopIndhold}>
-                <div className={mystyle.transparentContainer}></div>
-                <h1>SCALE</h1>
+                <img className={mystyle.forsideLogo} src="/src/assets/scaleLogo.svg" alt="Scale logo"/>
                 <p>Design med omtanke - Vi optimerer din digitale tilstedeværelse ved at kombinere æstetik, funktionalitet og performance. </p>
             </section>
             <article>
